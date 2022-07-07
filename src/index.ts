c4e05b0b5de035d2c2ebc709347aa916c95422ed
Ruixuan Huang <ryan_0149@qq.com>
2022-07-08 00:51:09 +0800

    services for study blog

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import cors from '@koa/cors';
+import router from './router';
+
+const app = new Koa();
+
+app.use(cors());
+app.use(bodyParser());
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+
+app.listen(4100);
